dc0fb4a7b3e2ce01bc76b40a178f3dd3c2be07f5
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-12-01 21:10:32 -0300

    feat: Funções em java

--- fixationExercises/fixationTwo/excercises/src/App.java
+++ fixationExercises/fixationTwo/excercises/src/App.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+// Funções em java
+
 public class App {
     public static void main(String[] args) throws Exception {
         // Operadores bitwise
@@ -14,6 +16,58 @@ public class App {
         } else {
             System.out.println("6th bit is false!");
         }
+
+        scanner.close();
+    }
+
+    public static void mainTwo(String[] args) throws Exception {
+        // Algumas funções uteis
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        // Descobrindo o maior numero com if 
+        if (a > b && a > c) {
+            System.out.println("Higher = " + a);
+        } else if (b > c) {
+            System.out.println("Higher = " + b);
+        } else {
+            System.out.println("Higher = " + c);
+        }
+
+        scanner.close();
+    }
+
+    public static int max(int a, int b, int c) {
+        int aux;
+        if (a > b && a > c) {
+            aux = a;
+        } else if (b > c) {
+            aux = b;
+        } else {
+            aux = c;
+        }
+        return aux;
+    }
+
+    public static void showResult(int value) {
+        System.out.println("Higher = " + value);
+    }
+
+    public static void mainThree(String[] args) throws Exception {
+        // Algumas funções uteis
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        // Descobrindo o maior numero através de callback 
+        int higher = max(a, b, c);
+
+        showResult(higher);
 
         scanner.close();
     }
