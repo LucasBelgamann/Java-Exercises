3f0e9bbf99b048501f6469c3aed859f5c0959408
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-12-02 23:45:44 -0300

    feat: implementação da classe programam

--- fixationExercises/fixationThree/objectOrientation/src/aplication/ProgamamTwo.java
+++ fixationExercises/fixationThree/objectOrientation/src/aplication/ProgamamTwo.java
@@ -1,5 +1,26 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class ProgamamTwo {
-    
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        product.name = scanner.nextLine();
+        System.out.print("Price: ");
+        product.price = scanner.nextDouble();
+        System.out.print("Quantity: ");
+        product.quantity = scanner.nextInt();
+
+        System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+
+        scanner.close();
+    }
 }
