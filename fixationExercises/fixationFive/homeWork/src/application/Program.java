353f10eec7fc509a9ba04cb38bc255a361a2984e
LucasBelgamann <lucasbelgamann@hotmail.com>
2023-01-18 19:53:40 -0300

    feat: ex de construtor completo

--- fixationExercises/fixationFive/homeWork/src/application/Program.java
+++ fixationExercises/fixationFive/homeWork/src/application/Program.java
@@ -8,20 +8,17 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Product product = new Product();
+
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Price: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
         System.out.print("Quantity in stock: ");
-        product.quantity = sc.nextInt();
-        System.out.println();
-        System.out.println("Product data: " + product);
-        System.out.println();
-        System.out.print("Enter the number of products to be added in stock: ");
         int quantity = sc.nextInt();
-        product.addProducts(quantity);
+        // Instância do construtor passando os atributos necessários
+        Product product = new Product(name, price, quantity);
+
         System.out.println();
         System.out.println("Updated data: " + product);
         System.out.println();
--- fixationExercises/fixationFive/homeWork/src/entities/Product.java
+++ fixationExercises/fixationFive/homeWork/src/entities/Product.java
@@ -5,6 +5,13 @@ public class Product {
     public double price;
     public int quantity;
 
+    // Construtor da classe
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
     public double totalValueInStock() {
         return price * quantity;
     }
